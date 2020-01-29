60d575a3348d17c5ded6389e5d7a7b201afe4b30
Manba Osamu <manba@zf7.so-net.ne.jp>
2020-01-30 08:49:11 +0900

    postデータをレスポンスに追加

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ async def root(req, resp):
   elif req.method == "post":
     data = await req.media()
     resp.media = data
-    resp.content = f"{(time.time() - start):6.3f} post"
+    resp.content = f"{(time.time() - start):6.3f} post {data}"
   else:
     resp.content = f"{(time.time() - start):6.3f} ?"
 
